807281e97a4a545a970b305153d6b709b41d08dd
Omar Hatim <Omarhatim79@hotmail.com>
2023-08-24 11:05:30 +0300

    m

--- lib/Theme/Color.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-final Color Primary = Color.fromARGB(255,54,0,255);
-final Color Secondary = Color.fromARGB(255,0,0,0);
-
-final Color Black = Color.fromARGB(255,0,0,0);
-final Color White = Color.fromARGB(255,238,238,238);
-final Color Transparent3 = Color.fromARGB(70,187, 187, 187);
-final Color Shadow = Color.fromARGB(40,21,21,21);
